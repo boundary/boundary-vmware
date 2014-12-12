d69f637ad6d3b5ff40bc474b30ce5adfe0d7401a
jdgwartney <davidg@boundary.com>
2014-12-12 00:20:31 -0800

    Add metho to create a default VMWare client

--- src/test/java/com/boundary/metrics/vmware/VMWareTestUtils.java
+++ src/test/java/com/boundary/metrics/vmware/VMWareTestUtils.java
@@ -4,16 +4,35 @@ import io.dropwizard.configuration.ConfigurationFactory;
 import io.dropwizard.jackson.Jackson;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Properties;
 
 import javax.validation.Validation;
 import javax.validation.Validator;
 
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import com.boundary.metrics.vmware.poller.VMwareClient;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.google.common.io.Resources;
+import com.vmware.connection.Connection;
 
 public class VMWareTestUtils {
+	
+	private final static String URL = "com.boundary.metrics.vmware.client.url";
+	private final static String USER = "com.boundary.metrics.vmware.client.user";
+	private final static String PASSWORD = "com.boundary.metrics.vmware.client.password";
+	
+	public final static String DEFAULT_VMWARE_CLIENT_CONFIGURATION = "vmware-client.properties";
+
+	private static Properties clientProperties;
 
 	public static VMwarePerfAdapterConfiguration getConfiguration(String resource) throws Exception {
 
@@ -39,5 +58,32 @@ public class VMWareTestUtils {
 		}
 		
 		return configuration;
+	}
+	
+	/**
+	 * Help function that creates a VMware Connection from the provide resource (from class path)
+	 * 
+	 * @param resource Properties file in the classpath
+	 * @return {@link Connection} VMware connection instance
+	 */
+	public static Connection getVMWareConnection(String resource) {
+		VMwareClient client = null;
+		try {
+			File propertiesFile = new File(Resources.getResource(resource).toURI());
+			Reader reader = new FileReader(propertiesFile);
+			clientProperties = new Properties();
+			clientProperties.load(reader);
+			final String url = clientProperties.getProperty(URL);
+			final String user = clientProperties.getProperty(USER);
+			final String password = clientProperties.getProperty(PASSWORD);
+
+			client = new VMwareClient(new URI(url), user, password);
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+
+		return client;
 	}
 }
