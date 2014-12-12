d6149a483f3a7795c2b98fa7715ba76387eca56d
jdgwartney <davidg@boundary.com>
2014-12-12 08:18:00 -0800

    Update JavaDocs and copyrights

--- src/main/java/com/boundary/metrics/vmware/client/client/metrics/MetricsClient.java
+++ src/main/java/com/boundary/metrics/vmware/client/client/metrics/MetricsClient.java
@@ -1,3 +1,17 @@
+// Copyright 2014 Boundary, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.boundary.metrics.vmware.client.client.metrics;
 
 import com.google.common.base.Charsets;
@@ -37,6 +51,12 @@ public class MetricsClient {
     private static final Joiner PATH_JOINER = Joiner.on('/');
     private static final Logger LOG = LoggerFactory.getLogger(MetricsClient.class);
 
+    /**
+     * Constructor
+     * @param client Jersey client
+     * @param baseUrl URI to make REST call
+     * @param authentication authentication string
+     */
     public MetricsClient(Client client, URI baseUrl, String authentication) {
         checkNotNull(client);
         checkNotNull(baseUrl);
@@ -45,6 +65,12 @@ public class MetricsClient {
         this.authentication = checkNotNull(authentication);
     }
 
+    /**
+     * Creates or updates a metric if the metric alrady exists
+     * 
+     * @param metricName
+     * @param unit
+     */
     public void createMetric(String metricName, String unit) {
         CreateUpdateMetric metricRequest = new CreateUpdateMetric(metricName, unit);
         ClientResponse response = baseResource.path(PATH_JOINER.join("v1", "metrics", metricRequest.getName()))
