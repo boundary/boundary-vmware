c8dc37c4a953a8825df004d580e5920cc2b4d8a8
jdgwartney <davidg@boundary.com>
2014-12-12 10:21:07 -0800

    Add additional tests ; add connection is the class setup

--- src/test/java/com/boundary/metrics/vmware/GetMOREFTest.java
+++ src/test/java/com/boundary/metrics/vmware/GetMOREFTest.java
@@ -1,6 +1,5 @@
 package com.boundary.metrics.vmware;
 
-import static com.boundary.metrics.vmware.VMWareTestUtils.DEFAULT_VMWARE_CLIENT_CONFIGURATION;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
@@ -10,13 +9,17 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static com.boundary.metrics.vmware.VMWareTestUtils.*;
 
 import com.vmware.connection.Connection;
 import com.vmware.connection.helpers.GetMOREF;
 import com.vmware.vim25.InvalidPropertyFaultMsg;
 import com.vmware.vim25.ManagedObjectReference;
 import com.vmware.vim25.RuntimeFaultFaultMsg;
+import com.vmware.vim25.TraversalSpec;
 
 /**
  * Regression tests for the {@GetMOREF} class
@@ -27,27 +30,47 @@ public class GetMOREFTest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
+		client = VMWareTestUtils.getVMWareConnection(DEFAULT_VMWARE_CLIENT_CONFIGURATION);
+		assertNotNull(client);
+		client.connect();
+		getMOREF = new GetMOREF(client);
+		assertNotNull(getMOREF);
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-	private GetMOREF getMOREF;
-	private Connection client;
+	private static GetMOREF getMOREF;
+	private static Connection client;
 	
 
 	@Before
 	public void setUp() throws Exception {
-		this.client = VMWareTestUtils.getVMWareConnection(DEFAULT_VMWARE_CLIENT_CONFIGURATION);
-		assertNotNull(this.client);
-		this.client.connect();
-		this.getMOREF = new GetMOREF(client);
-		assertNotNull(this.getMOREF);
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
+	}
+	
+	public Map<String, ManagedObjectReference> getManagedObjects(ManagedObjectReference folder, String type) throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+        ManagedObjectReference root = client.getServiceContent().getRootFolder();
+        return getManagedObjects(root,type);
+	}
+	
+	public Map<String, ManagedObjectReference> getManagedObjects(String type) throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+        ManagedObjectReference root = client.getServiceContent().getRootFolder();
+		Map<String, ManagedObjectReference> entities = getMOREF.inFolderByType(root,type);
+		assertNotNull(entities);
+		System.out.println("# " + type + ": " + entities.size());
+		
+		for (Map.Entry<String, ManagedObjectReference> entity : entities.entrySet()) {
+            ManagedObjectReference mor = entity.getValue();
+            String entityName = entity.getKey();
+            System.out.println(type + ": " + entityName);
+		}
+		return entities;
 	}
 
 	@Test
@@ -60,96 +83,71 @@ public class GetMOREFTest {
 	}
 
 	@Test
-	public void testContainerViewByTypeManagedObjectReferenceStringRetrieveOptions() {
-		//fail("Not yet implemented");
-	}
-
-	@Test
-	public void testContainerViewByTypeManagedObjectReferenceStringRetrieveOptionsStringArray() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testPropertyFilterSpecs() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testContainerViewByTypeManagedObjectReferenceStringStringArrayRetrieveOptionsPropertyFilterSpecArray() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testInFolderByTypeVirtualMachines() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
-        ManagedObjectReference root = client.getServiceContent().getRootFolder();
-		Map<String, ManagedObjectReference> entities = getMOREF.inFolderByType(root,"VirtualMachine");
-		assertNotNull(entities);
+		getManagedObjects("VirtualMachine");
 	}
 	
 	@Test
-	public void testInFolderByTypeVirtualMachines1() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+	public void testInFolderByTypeDatacenter() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+		getManagedObjects("Datacenter");
+	}
+	
+	@Ignore("Broken Test")
+	@Test
+	public void testInFolderByTypeHostSystem() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
         ManagedObjectReference root = client.getServiceContent().getRootFolder();
-		Map<String, ManagedObjectReference> entities = getMOREF.inFolderByType(root,"VirtualMachine");
+		Map<String, ManagedObjectReference> entities = getMOREF.inFolderByType(root,"Folder");
+		assertNotNull(entities);
+		System.out.println("# Folder: " + entities.size());
+		
+		ManagedObjectReference hosts = entities.get("host");
+		
+		getManagedObjects(hosts,"ComputeResource");
+		
+		entities = getMOREF.inFolderByType(hosts,"ComputeResource");
+		System.out.println("# ComputeResource: " + entities.size());
+		
+		for (Map.Entry<String, ManagedObjectReference> entity : entities.entrySet()) {
+            ManagedObjectReference mor = entity.getValue();
+            String entityName = entity.getKey();
+            System.out.println("ComputeResource: " + entity.getKey());
+		}
+		
+		entities = getMOREF.inFolderByType(hosts,"Folder");
+		System.out.println("# Folder: " + entities.size());
+		
+		for (Map.Entry<String, ManagedObjectReference> entity : entities.entrySet()) {
+            ManagedObjectReference mor = entity.getValue();
+            String entityName = entity.getKey();
+            System.out.println("Folder: " + entity.getKey());
+		}
 	}
-
+	
+	@Ignore("None of these managed objects on target")
 	@Test
-	public void testInContainerByTypeManagedObjectReferenceStringStringArrayRetrieveOptions() {
-		fail("Not yet implemented");
+	public void testInFolderByTypeComputeResource() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+		getManagedObjects("ComputeResources");
 	}
-
+	
 	@Test
-	public void testInContainerByTypeManagedObjectReferenceStringRetrieveOptions() {
-		fail("Not yet implemented");
+	public void testInFolderByTypeFolder() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+		getManagedObjects("Folder");
 	}
-
+	
 	@Test
-	public void testToMap() {
-		fail("Not yet implemented");
+	public void testInFolderByTypeNetwork() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+		getManagedObjects("Network");
 	}
-
+	
 	@Test
-	public void testPopulateRetrieveResultMapOfStringManagedObjectReference() {
-		fail("Not yet implemented");
+	public void testInFolderByTypeDatastore() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+		getManagedObjects("Datastore");
 	}
-
+	
 	@Test
-	public void testPopulateRetrieveResultListOfObjectContent() {
-		fail("Not yet implemented");
+	public void testVMTraversalSpec() throws RuntimeFaultFaultMsg, InvalidPropertyFaultMsg {
+		TraversalSpec ts = getMOREF.getVMTraversalSpec();
+		assertNotNull(ts);
+		
 	}
-
-	@Test
-	public void testVmByVMname() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetVMTraversalSpec() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEntityPropsManagedObjectReferenceStringArray() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testEntityPropsListOfManagedObjectReferenceStringArray() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testInContainerByTypeManagedObjectReferenceString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testInFolderByTypeManagedObjectReferenceString() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testInContainerByTypeManagedObjectReferenceStringStringArray() {
-		fail("Not yet implemented");
-	}
-
 }
