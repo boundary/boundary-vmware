868827b52e05cfa3376d192a819698074d9160a4
jdgwartney <davidg@boundary.com>
2014-12-12 15:30:56 -0800

    Stubbed tests the Metrics Client

---
+++ src/test/java/com/boundary/metrics/vmware/client/MetricsClientTest.java
@@ -0,0 +1,54 @@
+package com.boundary.metrics.vmware.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MetricsClientTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Ignore
+	@Test
+	public void testMetricsClient() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testCreateMetric() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testAddMeasurementsIntMapOfStringNumberOptionalOfDateTime() {
+		fail("Not yet implemented");
+	}
+
+	@Ignore
+	@Test
+	public void testAddMeasurementsListOfMeasurement() {
+		fail("Not yet implemented");
+	}
+
+}
