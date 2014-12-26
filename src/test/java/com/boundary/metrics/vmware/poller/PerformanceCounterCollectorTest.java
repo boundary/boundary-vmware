3d1c28ddcd68c491e83ba15e4dc9b1ce894e9ebe
jdgwartney <davidg@boundary.com>
2014-12-26 13:08:22 -0800

    Update tests ; add licenes

--- src/test/java/com/boundary/metrics/vmware/poller/PerformanceCounterCollectorTest.java
+++ src/test/java/com/boundary/metrics/vmware/poller/PerformanceCounterCollectorTest.java
@@ -1,10 +1,28 @@
+// Copyright 2014 Boundary, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package com.boundary.metrics.vmware.poller;
 
 import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 import java.net.URISyntaxException;
 import java.util.Map;
+import java.util.Properties;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -13,19 +31,32 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.boundary.metrics.vmware.client.VMWareClientFactory;
+import com.google.common.io.Resources;
 import com.vmware.connection.Connection;
 import com.vmware.vim25.InvalidPropertyFaultMsg;
 import com.vmware.vim25.PerfCounterInfo;
 import com.vmware.vim25.RuntimeFaultFaultMsg;
 
 public class PerformanceCounterCollectorTest {
+	
+	private final static String PERFORMANCE_COUNTER_PROPERTIES="vmware-performance-counters.properties";
+	private static Properties performanceCounters = null;
 
 	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
+	public static void setUpBeforeClass() {
+		try {
+			File propertiesFile = new File(Resources.getResource(PERFORMANCE_COUNTER_PROPERTIES).toURI());
+			Reader reader = new FileReader(propertiesFile);
+			performanceCounters = new Properties();
+			performanceCounters.load(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		performanceCounters = null;
 	}
 
 	@Before
@@ -36,6 +67,15 @@ public class PerformanceCounterCollectorTest {
 	public void tearDown() throws Exception {
 	}
 
+	/**
+	 * Tests collecting of Performance Counters against the vSphere end point. Compare the returned values against our
+	 * known list. Test will fail if we can not find the performance counter in our property list.
+	 * 
+	 * @throws URISyntaxException {@link URISyntaxException}
+	 * @throws IOException {@link IOException}
+	 * @throws InvalidPropertyFaultMsg {@link InvalidPropertyFaultMsg}
+	 * @throws RuntimeFaultFaultMsg {@link RuntimeFaultFaultMsg}
+	 */
 	@Test
 	public void testPerformanceCounterCollector() throws URISyntaxException, IOException, InvalidPropertyFaultMsg, RuntimeFaultFaultMsg {
 		Connection client = VMWareClientFactory.createClient();
@@ -48,7 +88,14 @@ public class PerformanceCounterCollectorTest {
 		Map<Integer,PerfCounterInfo> infoMap = metadata.getInfoMap();
 		
 		for (PerfCounterInfo info : infoMap.values()) {
-			System.out.printf("%s.%s.%s\n",info.getGroupInfo().getKey(),info.getNameInfo().getKey(),info.getRollupType().toString());
+			String key = String.format("%s.%s.%s",info.getGroupInfo().getKey(),info.getNameInfo().getKey(),info.getRollupType().toString());
+			boolean counter = Boolean.valueOf(performanceCounters.getProperty(key)).booleanValue();
+			assertEquals("check for performance counter info: " + key,true,counter);
+		}
+		
+		for (String name : nameMap.keySet()) {
+			boolean counter = Boolean.valueOf(performanceCounters.getProperty(name)).booleanValue();
+			assertEquals("check for performance counter name: " + name,true,counter);
 		}
 	}
 }
